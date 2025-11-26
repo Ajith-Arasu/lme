dc0756ceeddb27d9c74a2235cf2ede2387fe348b
Ajith <80077370+Ajith-Arasu@users.noreply.github.com>
2025-11-26 18:12:21 +0530

    build error fix

--- src/API/services/events.service.ts
+++ src/API/services/events.service.ts
@@ -3,8 +3,6 @@ import { EVENTS_URL } from "../urlConfig";
 
 export const getActiveExamEvents = async (
   university_id: string,
-  count: number | string = "",
-  page: number | string = ""
 ) => {
   const response = await api.get(EVENTS_URL, {
     params: {
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -31,6 +31,7 @@ const Login = () => {
         student_login_id: data.username,
         student_password: data.password,
       });
+      console.log("res===>",res)
       if (res?.statusCode === 200) {
         // Redirect to page
         navigate("/exam-events");
